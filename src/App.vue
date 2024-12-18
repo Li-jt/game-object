f4c2f93872d220621a6f07d2626d94dde1e52612
lijt <15226153903@163.com>
2024-12-18 13:59:40 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ onMounted(()=>{
   const userStore = useUserStore()
   if(!userStore.getItem(itemEnum.Fire) || userStore.getItem(itemEnum.Fire)?.level < 1){
     userStore.setItem(itemEnum.Fire,{
-      name: '火',
+      name: '火111111',
       icon: '🔥',
       type: itemEnum.Fire,
       level: 1,
